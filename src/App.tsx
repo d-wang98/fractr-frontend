f57aa826c4ad115936ea81eba7b00aadaaac567b
yaosusu <yaosusu@yaosusudembp.mynetworksettings.com>
2022-08-14 17:49:15 -0400

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Main from "./Main";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/settings">Settings</Link>
+          </li>
+          <li>
+            <Link to="/secondaryArt">Secondary Art</Link>
+          </li>
+        </ul>
+        <hr />
+        <Main />
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/SecondaryArt.tsx
@@ -0,0 +1,9 @@
+const SecondaryArt = () => {
+  return (
+    <div>
+      <h3>WELCOME TO FRACTR HOME</h3>
+      <p>This is the home page</p>
+    </div>
+  );
+};
+export default SecondaryArt;
